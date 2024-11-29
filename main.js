4c9787d721fcd925e076ff9e7eb87898d792962b
fridaSea <maria.kluge.codes@gmail.com>
2024-11-29 14:19:57 +0100

    Exercises 1-6

---
+++ main.js
@@ -0,0 +1,16 @@
+// Writing expressions with variables
+
+
+//EXERCISE 1
+//In your JavaScript file create a variable called myName with your name as the value. Put your name inside string quotes, e.g., "my name". 
+//Then add a line of code to print the variable name to the console after the previous message.
+//Save your JavaScript file in your editor. Reload the HTML page in your browser. You should see your name printed. If not, investigate and fix it.
+
+const myName = "Maria";
+console.log(myName);
+
+
+//EXERCISE 2
+//Create a variable called age with a number that is your age. Do not use string quotes for numbers.
+//Add a line to print that variable in the console. Save the file and reload the page. You should see your name and your age.
+
