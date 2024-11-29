bc4c9ae00384fd377dfa5a4096bff2bd91fcf524
fridaSea <maria.kluge.codes@gmail.com>
2024-11-29 14:20:01 +0100

    Exercieses 1-6

--- main.js
+++ main.js
@@ -14,3 +14,69 @@ console.log(myName);
 //Create a variable called age with a number that is your age. Do not use string quotes for numbers.
 //Add a line to print that variable in the console. Save the file and reload the page. You should see your name and your age.
 
+const numAge = 36;
+console.log(numAge);
+
+
+//EXERCISE 3
+//Create a variable called juliaAge with a value 32. 
+//Create another variable called ageDiff and set it to an expression that calculates your age minus Julias's age. Print the value of ageDiff. Save the file and reload the page. 
+//You should see your age and the age difference. If you are younger than Julia, you should see a negative number.
+
+const numJuliaAge = 32;
+const ageDiff = numAge - numJuliaAge;
+console.log(ageDiff);
+
+
+//Writing code with CONDITIONALS 
+//Conditionals are forms used in programming to tell the computer to do different things, depending on some test, e.g., "if the user is logged in, say "Hi", else say "Please log in."
+// The most basic conditional form is if...else...
+
+//EXERCISE 4
+// Write a conditional that compares the variable with your age with the number 21. 
+//It should print either "You are older than 21" or "You are not older than 21", appropriately, depending on your age. 
+//Save your JavaScript file and reload the page. Make sure you see the correct message. 
+//Try changing your age in the JavaScript file to make sure the other message prints when it should.
+
+if (numAge > 21) {
+    console.log("You are older than 21");
+} else { console.log("You are not older than 21")};
+
+
+//EXERCISE 5
+//Write a conditional that compares your age with Julia's age. 
+//This conditional will need to test if you are older, younger, or the same age, and print, appropriately, 
+//either "Julia is older than you", "Julia is younger than you", or "You have the same age as Julia". 
+//Save your changes and reload the file.
+
+if (numAge > numJuliaAge) {
+    console.log("Julia is younger than I am");
+}
+else if (numAge < numJuliaAge) {
+    console.log("Julia is older than I am");
+}
+else if (numAge === numJuliaAge) {
+        console.log("I have the same age as Julia");
+    }
+
+
+//JavaScript ARRAYS
+
+//EXERCISE 6
+//Create an array with all the names of your class (including mentors). 
+
+const classArray = ["Dima", "Diogo", "Hasan", "Jonas", "Maria", "Raúl", "Thomas"];
+// print the first element of the array in the console.
+console.log(classArray[0]);
+
+//print the last element of the array in the console.
+const lastName = classArray[classArray.length - 1];
+console.log(lastName);
+
+// print all the elements of the array in the console (use a "for" loop).
+
+for (let i = 0; i = classArray; i++) {
+    console.log(i);
+}
+
+
